f3c56a42dceed5ba8d13793aa612868fee167d5e
ankit375 <ankitdange37@gmail.com>
2025-12-17 16:39:40 +0530

    base-file/aircnms: changed registration url; CMD: added new fw upgrade url, fixed fw status

--- packages/aircnms/src/managers/cmdexecd/src/main.c
+++ packages/aircnms/src/managers/cmdexecd/src/main.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-    if (check_fw_upgrade_status()) {
-        LOG(INFO, "%s: CMDEXEC fw upgrade status sending", __func__);
-        cmdexec_send_event_to_cloud(UPGRADE, UPGRADED, NULL, NULL);
-        set_fw_upgrade_status_to_aircnms(UPGRADED);
-    }
-    
     // Initialize unixcomm server for async message handling
     if (!cmdexec_ubus_tx_service_init()) {
         LOG(ERR, "CMDEXEC: Failed to initialize ubus server");
         return -1;
+    }
+
+    if (check_fw_upgrade_status()) {
+        LOG(INFO, "%s: CMDEXEC fw upgrade status sending", __func__);
+        cmdexec_send_event_to_cloud(UPGRADE, UPGRADED, NULL, NULL);
+        set_fw_upgrade_status_to_aircnms(UPGRADED);
     }
     
     if (!cmdexec_ubus_rx_service_init()) {
